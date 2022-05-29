cab80dcb58006b0725578d7555b287188a7dab4f
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-05-29 12:23:32 -0700

    header files

---
+++ LSF.h
@@ -0,0 +1,8 @@
+#ifndef LSF_H
+#define LSF_H
+
+struct Point {
+    float x, y;
+};
+
+#endif
--- prog.cpp
+++ prog.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 
+#include "matrix.c"
+#include "LSF.h"
+
 using namespace std;
 
 int main() {
