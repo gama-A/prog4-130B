f5862eb64a848d5a1bf4fc83f6818605965a0cb1
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-06-02 17:24:59 -0700

    back to working on this

--- LSF.h
+++ LSF.h
@@ -1,8 +1,31 @@
 #ifndef LSF_H
 #define LSF_H
 
+#include <iostream>
+#include <vector>
+#include <random>
+
+extern "C" {
+#include "matrix.c"
+}
+
 struct Point {
-    float x, y;
+    double x, y;
 };
+
+void findLS(vector<Point> points) {
+    int numPoints, i, j;
+    int max_iterations = 1000;
+
+    vector<Point> selected;
+    
+    i = 0;
+    while(i < max_iterations) {
+        for(j = 0; j < numPoints; j++) {
+
+        }
+        
+    }
+}
 
 #endif
--- prog.cpp
+++ prog.cpp
@@ -1,12 +1,38 @@
 // Gamaliel Aristnodo, 8404071
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-#include "matrix.c"
 #include "LSF.h"
 
 using namespace std;
 
 int main() {
+    vector<Point> points;
+    double x, y;
+
+    string line, input;
+    ifstream file("test_prog4/input_1.txt");
+    if(file.is_open()) {
+        Point temp;
+        while(getline(file, line)) {
+            stringstream ss(line);
+            getline(ss, input, ' ');
+            istringstream(input) >> x;
+            getline(ss, input);
+            istringstream(input) >> y;
+
+            temp.x = x;
+            temp.y = y;
+
+            points.push_back(temp);
+        }
+    }
+    
+    findLS(points);
+
     return 0;
 }
