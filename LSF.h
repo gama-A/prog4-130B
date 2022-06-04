1c6b4757cc1b5ebd88db7afd498c8e70d36e3bc5
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-06-04 01:08:44 -0700

    part 1 almost done

--- LSF.h
+++ LSF.h
@@ -6,6 +6,7 @@
 #include <random>
 #include <algorithm>
 #include <cmath>
+#include <tuple>
 #include <omp.h>
 
 using namespace std;
@@ -14,15 +15,15 @@ struct Point {
     double x, y;
 };
 
-double calculateLine(Point p0, Point p1, Point p2) {
+tuple<double,double,double> calculateLine(vector<Point> s) {
     double A, B, C, denom;
 
-    denom = ((p0.x - p1.x)*(p0.x - p2.x))*(p1.x - p2.x);
-    A = (p2.x * (p1.y - p0.y) + p1.x * (p0.y - p2.y) + p0.x * (p2.y - p1.y)) / denom;
-    B = (pow(p2.x, 2.0) * (p0.y - p1.y) + pow(p1.x, 2.0) * (p2.y - p0.y) + pow(p0.x, 2.0) * (p1.y - p2.y)) / denom;
-    C = (p1.x * p2.x * (p1.x - p2.x) * p0.y + p2.x * p0.x * (p2.x - p0.x) * p1.y + p0.x * p1.x * (p0.x - p1.x) * p2.y) / denom;
+    denom = (s[0].x - s[1].x)*(s[0].x - s[2].x)*(s[1].x - s[2].x);
+    A = (s[2].x * (s[1].y - s[0].y) + s[1].x * (s[0].y - s[2].y) + s[0].x * (s[2].y - s[1].y)) / denom;
+    B = (pow(s[2].x, 2.0) * (s[0].y - s[1].y) + pow(s[1].x, 2.0) * (s[2].y - s[0].y) + pow(s[0].x, 2.0) * (s[1].y - s[2].y)) / denom;
+    C = (s[1].x * s[2].x * (s[1].x - s[2].x) * s[0].y + s[2].x * s[0].x * (s[2].x - s[0].x) * s[1].y + s[0].x * s[1].x * (s[0].x - s[1].x) * s[2].y) / denom;
 
-    return A, B, C;
+    return make_tuple(A, B, C);
 }
 
 double findDist(Point p, double A, double B, double C) {
@@ -33,7 +34,7 @@ double findDist(Point p, double A, double B, double C) {
 void findLS(vector<Point> points) {
     int numPoints, i, j, N;
     numPoints = points.size();
-    double A, B, C;
+    double A, B, C, A_ans, B_ans, C_ans;
 
     // N = The number of samples to get a 99% chance the line has no outliers
     N = ceil( log10(1 - 0.99) / log10(1 - pow(1-0.3, 3.0)) );
@@ -44,27 +45,32 @@ void findLS(vector<Point> points) {
         vector<Point> randomSamples(3);
         vector<Point> remainders = points;
         vector<std::mt19937> m_RandEngines;
-        Point p0, p1, p2;
+
+        int nThreads = max(1, omp_get_max_threads());
+		for (int i = 0; i < nThreads; ++i)
+		{				
+            random_device SeedDevice;
+			m_RandEngines.push_back(std::mt19937(SeedDevice()));
+		}
 
         shuffle(remainders.begin(), remainders.end(), m_RandEngines[omp_get_thread_num()]); // To avoid picking the same element more than once
 		copy(remainders.begin(), remainders.begin() + 3, randomSamples.begin());
+        remainders.erase(remainders.begin(), remainders.begin() + 3);
         
         // Calculate quadratic from 3 points
-        for(i = 0; i < 3; i++) {
-            if(i == 0) p0 = randomSamples[i];
+        tie(A, B, C) = calculateLine(randomSamples);
 
-            if(i == 1) p1 = randomSamples[i];
-
-            if(i == 2) p2 = randomSamples[i];
-        }
-
-        A, B, C = calculateLine(p0, p1, p2);
-
-        double error = 0.0;
+        double error = 0.0, prevError;
         for(auto p : remainders) {
             error += findDist(p, A, B, C);
         }
         error = error / (numPoints - 3);
+
+        if(j != 0) {
+            if(error < prevEror)
+                A_ans = A, B_ans = B, C_ans = C;
+        }
+        prevError = error
 
         j++;
     }
